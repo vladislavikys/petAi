d4803cbf29a7b837495b073e384fd1a9e359155c
Влад <zarya.vlad@yandex.ru>
2024-01-14 10:51:38 +0300

    init

---
+++ petsAIUITests/petsAIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  petsAIUITestsLaunchTests.swift
+//  petsAIUITests
+//
+//  Created by Влад on 13.01.24.
+//
+
+import XCTest
+
+final class petsAIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
